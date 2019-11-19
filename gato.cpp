eb3fa9256fbe497c4bc095ee345f346b2ab6c463
urbinna00 <54686469+urbinna00@users.noreply.github.com>
2019-11-18 18:02:27 -0600

    Update gato.cpp

--- gato.cpp
+++ gato.cpp
@@ -1,52 +1,48 @@
 #include<iostream>
 #include<stdio.h>
 #include<string.h>
-#include <vector>
+#include<vector>
 
 using namespace std;
 
-int i,j,ganador;
-
-
-
 bool mifunc(vector< vector<char> > array,int n){
 	char aux;
-	int j = 0;
+	
+	bool ganador_encontrado=false;
 	for(int i =0;i<n;i++){
-		aux = array[i][j];
-		for(j =0;j<n;j++){
+		aux = array[i][0];
+		for(int j =0;j<n;j++){
 			if(aux==array[i][j]&&array[i][j]!=' '){
 				aux==array[i][j];
 			}
 			else{
 				break;
 			}
-			if(j==n-1) return true;
+			if(j==n-1) ganador_encontrado=true;
 		}
-		j = 0;
+	
 	}
 	
-	return false;
+	return ganador_encontrado;
 }
 
 bool mifunc2(vector< vector<char> > array,int n){
 	char aux;
-	
-	for(int i =0;i<n;i++){
-		aux = array[j][i];
-		for(int j =0;j<n;j++){
-			if(aux==array[j][i]&&array[j][i]!=' '){
-				aux==array[j][i];
+	bool ganador_encontrado=false;
+	for(int j =0;j<n;j++){
+		aux = array[0][j];
+		for(int i =0;i<n;i++){
+			if(aux==array[i][j]&&array[i][j]!=' '){
+				aux==array[i][j];
 			}
 			else{
 				break;
 			}
-			if(i==n-1) return true;
+			if(i==n-1) ganador_encontrado=true;
 		}
-		i = 0;
 	}
 	
-	return false;
+	return ganador_encontrado;
 }
 
 void imprimir_gato(vector< vector<char> > gato,int n){
@@ -66,10 +62,11 @@ void imprimir_gato(vector< vector<char> > gato,int n){
 int main(){
 	int n;
 	int k=1,fila,columna,opcion;
+	int ganador = 0;
 	cout<<"    EL JUEGO DE EL GATO (X/0)\n";
 	cout<<"formas de ganar: "<<endl;
 	cout<<"Ingrese el tamanio de su tablero (minimo una matriz de 3x3, solo es necesario ingresar un numero entero): "; cin >> n;
-	vector<vector<char>> gato(n,vector<char>(n,' '));
+	vector<vector<char> > gato(n,vector<char>(n,' '));
 	
 	do{
 		imprimir_gato(gato,n);
@@ -84,6 +81,7 @@ int main(){
 					cin>>columna;
 					if (fila>n ||columna>n){
 						cout<<"\nError en la cordenada";
+						cout<<"\n";
 					}	
 				}
 			while(fila>n ||columna>n);
@@ -91,10 +89,13 @@ int main(){
 			while (gato[fila-1][columna-1]!=' ');
 			gato[fila-1][columna-1]='X';
 			imprimir_gato(gato,n);
-			mifunc(gato,n);
-
+			cout << (mifunc(gato,n) ? "Ganador encontrado" : "Todavia no hay ganador") << endl;
+			cout << (mifunc2(gato,n) ? "Ganador encontrado" : "Todavia no hay ganador") << endl;
+			if(mifunc(gato,n) || mifunc2(gato,n))
+				ganador = 1;
+			k=2;
 		}
-		if (k%2 == 0){
+		else if (k%2 == 0){
 			do{
 			do{
 			cout<<"\nTurno del jugador 2";
@@ -103,6 +104,7 @@ int main(){
 			cin>>columna;
 			if(fila>n ||columna>n){
 				cout<<"\nError en la coordenada";
+				cout<<"\n";
 			}
 			}
 			while (fila>n||columna>n);
@@ -110,14 +112,18 @@ int main(){
 			while(gato[fila-1][columna-1]!=' ');
 			gato[fila-1][columna-1]='O';
 			imprimir_gato(gato,n);
-			mifunc(gato,n);
-			mifunc2(gato,n);
-
+			cout << (mifunc(gato,n) ? "Ganador encontrado" : "Todavia no hay ganador") << endl;
+			cout << (mifunc2(gato,n) ? "Ganador encontrado" : "Todavia no hay ganador") << endl;
+			if(mifunc(gato,n) || mifunc2(gato,n))
+				ganador = 2;
+			k=1;
+			
 		}
-		k=k+1;
+		cout << "Termina do while" << endl;
 		}
-		while(k--&&ganador==0);
-		if (mifunc(gato,n)==true||mifunc2(gato,n)==true){
+		while(ganador==0);
+		
+		if (ganador==1){
 			cout<<"\nEl ganador es el jugador 1 ";
 		}
 		if(ganador==2){
